c3d64db7bfa62e4d556bc65273ed9bdff97f19bc
Vladimir Belousov <vbelouso@redhat.com>
2023-05-03 17:28:11 +0300

    another

--- cmd/coffeshop/coffeshop.go
+++ cmd/coffeshop/coffeshop.go
@@ -4,33 +4,27 @@ import (
 	"context"
 	"fmt"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/vbelouso/coffeshop/db"
-	"github.com/vbelouso/coffeshop/internal/config"
+	"github.com/vbelouso/coffeshop/internal/db"
 	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
 type application struct {
 	q *db.Queries
-	c *config.Config
+	c *Config
 }
 
 //go:generate swagger generate spec --scan-models -o docs/swagger.yaml
 func main() {
-	cfg := config.InitializeConfig()
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
-
-	conn, err := openDB(dsn)
+	cfg := InitializeConfig()
+	conn, err := openDB(cfg.DBDSN)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
-	//conn := openDB(dsn)
 	queries := db.New(conn)
-
 	app := &application{q: queries, c: cfg}
 	srv := &http.Server{
 		Addr:         cfg.ServerPort,
@@ -43,22 +37,18 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-//func openDB(dsn string) (*pgx.Conn, error) {
-//	conn, err := pgx.Connect(context.Background(), dsn)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	return conn, conn.Ping(context.Background())
-//}
-
 func openDB(dsn string) (*pgxpool.Pool, error) {
 	ctx := context.Background()
-	pool, err := pgxpool.New(ctx, dsn)
+	config, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
 		return nil, err
 	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, fmt.Errorf("database connection error: %w", err)
+	}
+
 	defer pool.Close()
 	return pool, pool.Ping(ctx)
 }
